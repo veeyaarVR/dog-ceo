f1ab7d8e5c305822e5ebad0128ea54351f2ad2fa
Vignesh Marimuthu <vignesh.m@cumulations.com>
2024-06-04 18:25:06 +0530

    code optimise

--- lib/features/image/presentation/widgets/image_container.dart
+++ lib/features/image/presentation/widgets/image_container.dart
@@ -12,14 +12,7 @@ class ImageContainer extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Stack(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            child: Container(
-              height: 400,
-              width: double.infinity,
-              color: Colors.blueGrey[100],
-            ),
-          ),
+          _placeHolderColor(),
           ClipRRect(
             borderRadius: BorderRadius.all(Radius.circular(10)),
             child: FadeInImage.memoryNetwork(
@@ -31,6 +24,18 @@ class ImageContainer extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  /// shows a basic color until the image fades in and loads
+  Widget _placeHolderColor() {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      child: Container(
+        height: 400,
+        width: double.infinity,
+        color: Colors.blueGrey[100],
       ),
     );
   }
