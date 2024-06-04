fd13018aa4ec1c4c3a00198156531ff7a905869a
Vignesh Marimuthu <vignesh.m@cumulations.com>
2024-06-04 12:24:49 +0530

    removed unwanted try catch block

--- lib/features/subBreed/data/sources/remote/sub_breed_network_source.dart
+++ lib/features/subBreed/data/sources/remote/sub_breed_network_source.dart
@@ -1,7 +1,6 @@
 import 'package:dog_ceo/core/constants/constants.dart';
 import 'package:dog_ceo/core/resources/data_state.dart';
 import 'package:dog_ceo/core/resources/dio_client.dart';
-import 'package:flutter/cupertino.dart';
 
 class SubBreedNetworkSource {
   final DioClient dio;
@@ -15,27 +14,21 @@ class SubBreedNetworkSource {
 
       if (dio.isApiSuccess(response)) {
         /// if success, get data from message object
-
-        try {
-          List<dynamic> subBreedList = response["message"];
-          List<String> dataList = [];
-          for (var element in subBreedList) {
-            dataList.add(element.toString());
-          }
-          if (dataList.isNotEmpty) {
-            return DataSuccess(dataList);
-          } else {
-            return DataFailed(errorMessage: "No Sub-breeds found");
-          }
-        } on TypeError {
-          return DataFailed(errorMessage: "No Sub-breeds found");
-        } catch (e) {
-          debugPrint("${e.runtimeType}  ${e.toString()}");
+        List<dynamic> subBreedList = response["message"];
+        List<String> dataList = [];
+        for (var element in subBreedList) {
+          dataList.add(element.toString());
+        }
+        if (dataList.isNotEmpty) {
+          return DataSuccess(dataList);
+        } else {
           return DataFailed(errorMessage: "No Sub-breeds found");
         }
       } else {
         return DataFailed(errorMessage: "Couldn't fetch sub breeds now.");
       }
+    } on TypeError {
+      return DataFailed(errorMessage: "No Sub-breeds found");
     } catch (e) {
       return DataFailed(errorMessage: e.toString());
     }
